1900de588db568a53b57b67ae37cc911839684d1
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-08-20 14:44:52 +0900

    20210820-2
    conditionals-1

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const calculator = {
-    add : function(a,b){
-        return a+b;
-    },
-    minus : function(a,b){
-        return a-b;
-    },
-    divide : function(a,b){
-        return a/b;
-    },
-    powerOf : function(a,b){
-        return a**b;
-    }
-}
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.add(2,3);
-const minusResult = calculator.minus(plusResult,10);
-const timeResult = calculator.divide(10,minusResult);
-const powerResult = calculator.powerOf(timeResult,2);
+console.log(age);
