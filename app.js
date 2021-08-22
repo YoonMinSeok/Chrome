7ff65afc256900dcd9d5e8a1d9f837a213b3f7f6
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-08-22 16:49:26 +0900

    20210822-5
    login form

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-const title = document.querySelector("div.hello:first-child h1");
+const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick(){
-    title.style.color = "blue";
+function onLoginBtnClick(){
+    console.dir(loginInput.value);
 }
 
-function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOK");
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
+loginButton.addEventListener("click",onLoginBtnClick);
